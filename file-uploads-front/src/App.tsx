b299b7cea0f3fc924c9ef43933e843aa91bf5819
Oatsinthepine <zzy20001201@gmail.com>
2025-07-10 16:04:51 +1000

    Add initial scaffold setup for React + TypeScript + Vite before the file upload practice.

---
+++ file-uploads-front/src/App.tsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import './App.css'
+import FileForm from "./components/FileForm.tsx";
+
+function App() {
+
+  return (
+    <>
+      <div>
+          <FileForm />
+      </div>
+    </>
+  )
+}
+
+export default App
